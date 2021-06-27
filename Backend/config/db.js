ad21ca47b0571bf3b181279c85442a99c8096119
Sayan Roy <rsayan553@gmail.com>
2021-06-27 14:57:56 +0530

    performance improved

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require('sequelize');
 
-// const sequelize = new Sequelize('campusx', 'sayan', 'thisispassword$123', {
-//     dialect: 'mysql',
-//     host: 'localhost',
-// });
-const sequelize = new Sequelize('campusx', 'root', '', {
+const sequelize = new Sequelize('campusx', 'sayan', 'thisispassword$123', {
     dialect: 'mysql',
     host: 'localhost',
 });
+// const sequelize = new Sequelize('campusx', 'root', '', {
+//     dialect: 'mysql',
+//     host: 'localhost',
+// });
 
 module.exports = sequelize;
